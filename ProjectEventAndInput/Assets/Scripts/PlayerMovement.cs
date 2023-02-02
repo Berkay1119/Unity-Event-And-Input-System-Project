2a26d49774a0b9dfe154b63f7f53fdc3eeb21d46
Berkay Karakaya <berkay11karakaya@gmail.com>
2023-02-02 18:21:24 +0300

    blendTreeStarted

--- ProjectEventAndInput/Assets/Scripts/PlayerMovement.cs
+++ ProjectEventAndInput/Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,10 @@ public class PlayerMovement : MonoBehaviour
     private InputAction.CallbackContext _context;
     [SerializeField] private float speed=5f;
     private PlayerInput playerInput;
+    [SerializeField] private Animator _animator;
+    private float acceleration = 1f;
+    private float velocityY;
+    private float velocityX;
 
     private void Awake()
     {
@@ -18,13 +22,28 @@ public class PlayerMovement : MonoBehaviour
         playerInput = new PlayerInput();
         playerInput.Player.Enable();
         playerInput.Player.Jump.performed += Jump;
-
     }
 
     private void FixedUpdate()
     {
         Vector2 direction = playerInput.Player.Movement.ReadValue<Vector2>();;
-        _rigidbody.AddForce(new Vector3(direction.x,0,direction.y)*speed,ForceMode.Force);
+        //direction=direction.normalized;
+        transform.position += Time.deltaTime*new Vector3(direction.x, 0, direction.y);
+        if (direction!=Vector2.zero)
+        {
+            velocityY += Time.deltaTime * acceleration*direction.normalized.y;
+            velocityX += Time.deltaTime * acceleration*direction.normalized.x;
+            
+        }
+        else
+        {
+            velocityY = 0;
+            velocityX = 0;
+        }
+
+        _animator.SetFloat("Velocity Z",velocityY);
+        _animator.SetFloat("Velocity X",velocityX);
+        
     }
     
     public void Jump(InputAction.CallbackContext context)
